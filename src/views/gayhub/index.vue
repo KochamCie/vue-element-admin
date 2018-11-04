53a001531c48f234f6de8f9d631feda7c8e373d4
renns <745258662@qq.com>
2018-11-04 16:56:25 +0800

    gayhub module ， tree ui

--- src/views/gayhub/index.vue
+++ src/views/gayhub/index.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="tab-container">
 
+    <el-tag
+      :key="tag"
+      v-for="tag in dynamicTags"
+      closable
+      :disable-transitions="false"
+      @close="handleClose(tag)">
+      {{tag}}
+    </el-tag>
+    <el-input
+      class="input-new-tag"
+      v-if="inputVisible"
+      v-model="inputValue"
+      ref="saveTagInput"
+      size="small"
+      @keyup.enter.native="handleInputConfirm"
+      @blur="handleInputConfirm"
+    >
+    </el-input>
+    <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+
+
+
     <div class="filter-container">
 
       <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary"
@@ -89,6 +111,9 @@
     },
     data() {
       return {
+        dynamicTags: ['标签一', '标签二', '标签三'],
+        inputVisible: true,
+        inputValue: '',
         activeName: 'CN',
         createdTimes: 0,
         gitRepoForm: {
@@ -180,6 +205,25 @@
           repoName: ''
         }
       },
+      handleClose(tag) {
+        this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+      },
+
+      showInput() {
+        this.inputVisible = true
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus()
+        })
+      },
+
+      handleInputConfirm() {
+        var inputValue = this.inputValue
+        if (inputValue) {
+          this.dynamicTags.push(inputValue)
+        }
+        this.inputVisible = false
+        this.inputValue = ''
+      },
       handleCreate() {
         // this.resetModuleForm()
         this.dialogStatus = 'create'
@@ -216,5 +260,21 @@
 <style scoped>
   .tab-container {
     margin: 30px;
+  }
+
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
   }
 </style>
